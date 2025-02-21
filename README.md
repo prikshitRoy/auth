c3d01e8a8238a7a696bcac108f0387924e2ac345
prikshitRoy <parikshitroy00@gmail.com>
2025-02-22 01:34:51 +0530

    add keysely

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # How to Use Firebase Auth in Nextjs SSR
 
 **Tech Stack**
-Nextjs-14 , Firebase
+Nextjs-14 , Firebase , trpc , zod, kysely , prettier
+
+trpc:
+
+`yarn add @trpc/server@next @trpc/client@next @trpc/react-query@next @trpc/next@next @tanstack/react-query@latest zod`
+
+keysely:
+`yarn add kysely`
+`yarn add -D keysely-codegen`
--- src/app/firebase/firebase.ts
+++ src/app/firebase/firebase.ts
@@ -1,13 +1,6 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBXBfYrByml-441e3_8UFB5xerY0DwMZJ0",
-  authDomain: "fir-auth-in-ssr.firebaseapp.com",
-  projectId: "fir-auth-in-ssr",
-  storageBucket: "fir-auth-in-ssr.firebasestorage.app",
-  messagingSenderId: "675666350287",
-  appId: "1:675666350287:web:7633035f1a183ddf44e7ba",
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
