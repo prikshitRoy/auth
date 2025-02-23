eadbb404d936faefa1c223651132a199c5a48250
prikshitRoy <parikshitroy00@gmail.com>
2025-02-23 10:22:16 +0530

    feat:sign button and reciving access token

---
+++ src/app/_components/sign-in-button/sign-in-button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { signInWithGoogle } from "@/lib/configs/firebase-config";
+
+export const SignInButton: React.FC = () => {
+    async function handleSignIn(): Promise<void>{
+        try {
+
+            const userCredentials = await signInWithGoogle();
+            const accessToken = await userCredentials.user.getIdToken();            
+            console.log("Access Token",accessToken);
+        } catch (error: unknown) {
+            console.error(error);
+        }
+    }
+
+    return(
+        <button type="button" className="p-1 bg-blue-400 rounded-md flex" onClick={handleSignIn}>Sign in with Google</button>
+    )
+    
+}
--- src/app/firebase/firebase.ts
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-
-const firebaseConfig = {};
-
-const app = initializeApp(firebaseConfig);
