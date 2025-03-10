c714651d3be50847b6e67d0c1b50fda7b257c6b8
prikshitRoy <parikshitroy00@gmail.com>
2025-03-10 22:03:10 +0530

    fix:register user

--- basic-auth/nextjs-v14_with_next-auth-v4/src/app/layout.tsx
+++ basic-auth/nextjs-v14_with_next-auth-v4/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import Header from "@/components/Header";
+import ToasterContext from "@/components/ToasterContext";
 import "./globals.css";
 
 const geistSans = localFont({
--- basic-auth/nextjs-v14_with_next-auth-v4/src/app/page.tsx
+++ basic-auth/nextjs-v14_with_next-auth-v4/src/app/page.tsx
@@ -2,11 +2,13 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Home() {
+  // Fetch the server session using NextAuth
   const session = await getServerSession(authOptions);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] ">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <pre>{JSON.stringify(session)}</pre>
+        <pre className="w-fit">{JSON.stringify(session, null, 2)}</pre>
       </main>
     </div>
   );
