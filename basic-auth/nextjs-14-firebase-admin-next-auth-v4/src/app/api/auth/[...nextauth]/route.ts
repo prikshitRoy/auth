4cfb2dceb5eccc6d6845b52f9651723212fa77cc
prikshitRoy <parikshitroy00@gmail.com>
2025-05-02 12:56:53 +0530

    simple OAuth SetUp

--- basic-auth/nextjs-14-firebase-admin-next-auth-v4/src/app/api/auth/[...nextauth]/route.ts
+++ basic-auth/nextjs-14-firebase-admin-next-auth-v4/src/app/api/auth/[...nextauth]/route.ts
@@ -24,60 +24,14 @@ export const authOptions = {
   debug: process.env.NODE_ENV === "development",
   providers: [
     GithubProvider({
-      profile(profile) {
-        console.log("Provide from Github OAuth:", profile);
-
-        let userRole = "Github User";
-        if (profile?.email === "parikshitroy00@gmail.com") {
-          userRole = "admin";
-        }
-
-        return {
-          ...profile,
-          // NOTE: Convert profile.id to a string to avoid type mismatch errors
-          id: profile.id.toString(),
-          role: userRole,
-        };
-      },
       clientId: getCredentials("GITHUB").clientId,
       clientSecret: getCredentials("GITHUB").clientSecret,
     }),
     GoogleProvider({
-      profile(profile) {
-        console.log("Provide from Google OAuth:", profile);
-
-        let userRole = "Github User";
-        if (profile?.email === "parikshitroy01@gmail.com") {
-          userRole = "admin";
-        }
-
-        return {
-          ...profile,
-          // NOTE: The returned object is missing the 'id' property.
-          // Explicitly include an 'id' field using 'profile.sub',
-          // which typically represents the unique user ID in Google OAuth.
-          id: profile.sub,
-          role: userRole,
-        };
-      },
       clientId: getCredentials("GOOGLE").clientId,
       clientSecret: getCredentials("GOOGLE").clientSecret,
     }),
   ],
-  callbacks: {
-    async jwt({ token, user }: { token: JWT; user: User }) {
-      if (user) {
-        token.role = user.role;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      if (session.user) {
-        session.user.role = token.role;
-      }
-      return session;
-    },
-  },
 };
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- basic-auth/nextjs-14-firebase-admin-next-auth-v4/src/app/page.tsx
+++ basic-auth/nextjs-14-firebase-admin-next-auth-v4/src/app/page.tsx
@@ -2,6 +2,11 @@ import ServerSession from "@/components/UserSession/UserSession";
 
 import UserInfo from "@/components/UserInfo/UserInfo";
 
+const Learn = [
+  "Learn how to add Custom data like role to User.",
+  "Learn how to pass Custom data to user Session.",
+];
+
 export default async function Home() {
   return (
     <div className="flex flex-col px-2 w-fit h-full">
@@ -9,9 +14,18 @@ export default async function Home() {
         <h1 className="text-2xl underline font-bold">
           Next-Auth-v4 with Firebase Admin SDK
         </h1>
+
         <div className="bg-gray-600">
           TechStack: Nextjs-v14.2.25 + Next-Auth-v4 + Firebase
         </div>
+
+        <ul>
+          {Learn.map((item, index) => (
+            <li key={index}>
+              <h2 className="text-1xl text-green-400 font-bold">- {item}</h2>
+            </li>
+          ))}
+        </ul>
       </div>
       <ServerSession />
       <UserInfo />
